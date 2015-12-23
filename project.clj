9bfbf952437db4f43cc26e7b11d95dc9f0db8170
Paul Stadig <paul@stadig.name>
2015-12-23 08:41:34 -0500

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pjstadig/assertions "0.2.0-SNAPSHOT"
+(defproject pjstadig/assertions "0.2.0"
   :description "Zero penalty runtime assertions for Clojure."
   :url "http://github.com/pjstadig/assert"
   :license {:name "Mozilla Public License, v. 2.0"
