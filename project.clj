29118cc121349489661f4688612fd0de8bed1c0a
Paul Stadig <paul@stadig.name>
2015-12-23 08:41:15 -0500

    Add clojars as release repository.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://github.com/pjstadig/assert"
   :license {:name "Mozilla Public License, v. 2.0"
             :url "http://mozilla.org/MPL/2.0/"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :java-source-paths ["src"])
